397cd13fc1da72a6dc6e5e07b56cb02a78ab3052
JacksonSeowJX <seowjiaxian@gmail.com>
2024-11-11 03:46:36 +0800

    add tests

--- src/test/java/com/ntu/hns/admin/AdminActionsTest.java
+++ src/test/java/com/ntu/hns/admin/AdminActionsTest.java
@@ -2,21 +2,36 @@ package com.ntu.hns.admin;
 
 import static com.ntu.hns.TestManager.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AdminActionsTest {
 
-  @AfterEach()
-  public void tearDown() {
-    stopApplication();
+  @Test
+  @DisplayName("Test Case 20: View and Manage Hospital Staff ")
+  public void testViewAndManageHospitalStaff() {
+    provideInput("login\nA0001\n123\n1\n2\n3\n2\n1\n100\n\n5\nexit\n");
+    startApplication();
   }
 
   @Test
   @DisplayName("Test Case 21: View Appointment Details")
   public void testViewAppointmentDetails() {
-    provideInput("A0001\n123\n2\n");
+    provideInput("login\nA0001\n123\n2\n1\n2\n2\n2\n3\n2\n4\n2\n5\n5\nexit\n");
+    startApplication();
+  }
+
+  @Test
+  @DisplayName("Test Case 22: View and Manage Medication Inventory ")
+  public void testViewAndManageInventory() {
+    provideInput("login\nA0001\n123\n3\n2\n");
+    startApplication();
+  }
+
+  @Test
+  @DisplayName("Test Case 23: Approve Replenishment Requests ")
+  public void testApproveReplenishmentRequests() {
+    provideInput("login\nA0001\n123\n4");
     startApplication();
   }
 }
